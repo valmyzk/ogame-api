ff4c621270b3f435a026a189d09a18c0a7b7aaf4
ValentiMS <30903958+ValentiMS@users.noreply.github.com>
2019-09-21 17:21:45 +0200

    Fix planet report test

--- src/tests/report/planet.ts
+++ src/tests/report/planet.ts
@@ -2,11 +2,12 @@ import test from "ava";
 import PlanetReport from "../../report/planet";
 import LocalizationData from "../../localization/localizationData";
 import { Coords } from "../../universe/coords";
+import Universe from "../..";
 
 const sampleData = "coords;0:0:0|0;0|1;2";
 test.serial("constructor", t => {
 
-    t.notThrows(() => new PlanetReport(sampleData));
+    t.notThrows(() => new PlanetReport(sampleData, new Universe(800, "en")));
 
 });
 
@@ -57,7 +58,7 @@ test.serial("getUniverseLocalizations", async t => {
 
 test.serial("toString", t => {
 
-    const report = new PlanetReport(sampleData);
+    const report = new PlanetReport(sampleData, new Universe(800, "en"));
     
     t.deepEqual(report.toString(), sampleData);
 
